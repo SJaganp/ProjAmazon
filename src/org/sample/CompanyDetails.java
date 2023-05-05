9729773aa1a77b6a8bc013715fdb41100fb52eff
SJaganp <s.jagan2008@gmail.com>
2023-05-05 11:42:09 +0530

    Dev done:

--- src/org/sample/CompanyDetails.java
+++ src/org/sample/CompanyDetails.java
@@ -2,14 +2,14 @@ package org.sample;
 
 //child/subclass
 public class CompanyDetails {
+	
+	private void compId() {
+		System.out.println("Id : 65765");
+	}
 
 	public void compName() {
 		System.out.println("Comp name");
 	}
 
-	private void comLoc() {
-
-		System.out.println("Chennai");
-	}
-
+	
 }

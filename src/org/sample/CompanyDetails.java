fe44598d8f3633ce51537dd9827e42f554ac6359
SJaganp <s.jagan2008@gmail.com>
2023-05-05 11:31:33 +0530

    message

---
+++ src/org/sample/CompanyDetails.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+//child/subclass
+public class CompanyDetails {
+
+	public void compName() {
+		System.out.println("Comp name");
+	}
+
+	private void comLoc() {
+
+		System.out.println("Chennai");
+	}
+
+}
